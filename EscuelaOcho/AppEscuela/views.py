cdaf447e08056aa144510e52cc2781ce73205ac5
DiegoGalli <dhgalli@gmail.com>
2022-09-12 20:14:40 -0300

    *agregamos templates base y organizamos URLs

--- EscuelaOcho/AppEscuela/views.py
+++ EscuelaOcho/AppEscuela/views.py
@@ -9,3 +9,18 @@ def curso(request):
     curso.save()
     texto=f'Clase creada: nombre: {curso.nombre} comision= {curso.comision}'
     return HttpResponse(texto)
+
+def inicio(request):
+    return render (request, "AppEscuela/inicio.html")
+
+def cursos(request):
+    return render(request, "AppEscuela/cursos.html")
+
+def profesores(request):
+    return render(request, "AppEscuela/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppEscuela/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppEscuela/entregables.html")
--- EscuelaOcho/EscuelaOcho/urls.py
+++ EscuelaOcho/EscuelaOcho/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from AppEscuela.views import curso
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('curso/', curso),
+    path("AppEscuela/", include('AppEscuela.urls')),
+
+
 ]
