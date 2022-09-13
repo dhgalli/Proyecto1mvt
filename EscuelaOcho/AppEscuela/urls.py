92e33b18acd1208275cb6cd042c20fc4987109a9
DiegoGalli <dhgalli@gmail.com>
2022-09-12 21:36:13 -0300

    *agregamos de todo

--- EscuelaOcho/AppEscuela/urls.py
+++ EscuelaOcho/AppEscuela/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 urlpatterns = [ 
     path("curso/", curso),
-    path("profesores/", profesores),
-    path("estudiantes/", estudiantes),
-    path("entregables/", entregables),  
-    path("cursos/", cursos),
-    path("", inicio), 
+    path("profesores/", profesores, name='profesores'),
+    path("estudiantes/", estudiantes, name='estudiantes'),
+    path("entregables/", entregables, name='entregables'),  
+    path("cursos/", cursos, name='cursos'),
+    path("", inicio, name='inicio' ), 
 
 ]
